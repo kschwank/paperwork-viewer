9a8860f92f6423259fd6f6844adcc4e54ceafc49
kschwank <karsten.schwank@gmail.com>
2015-02-03 11:24:22 +0100

    scale images on load to reduce memory usage

--- paperwork-viewer/src/main/java/org/ako/pwv/controller/PagesAdapter.java
+++ paperwork-viewer/src/main/java/org/ako/pwv/controller/PagesAdapter.java
@@ -1,12 +1,14 @@
 package org.ako.pwv.controller;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.graphics.Point;
+import android.preference.PreferenceScreen;
+import android.view.*;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.Toast;
 import org.ako.pwv.R;
 import org.ako.pwv.view.PageViewHolder;
 
@@ -55,7 +57,28 @@ public class PagesAdapter extends BaseAdapter {
         }
 
         File imageFile = (File)getItem(position);
-        viewHolder.imageView.setImageBitmap(BitmapFactory.decodeFile(imageFile.getAbsolutePath()));
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+
+        WindowManager wm = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int maxWidth = size.x;
+
+        int scale = 1;
+        if (options.outWidth > maxWidth) {
+            scale = (int)Math.pow(2, (int)Math.ceil(Math.log(maxWidth / (double)options.outWidth) / Math.log(0.5)));
+        }
+
+        options = new BitmapFactory.Options();
+        options.inPurgeable = true;
+        options.inInputShareable = true;
+        options.inScaled = true;
+        options.inSampleSize = scale;
+        Bitmap srcBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+        viewHolder.imageView.setImageBitmap(srcBitmap);
 
         return convertView;
     }
